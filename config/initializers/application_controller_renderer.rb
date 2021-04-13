182f280dd2244aaf2f2e43e452cb6f6381d031f8
TajimaRyosuke <east.helman56@gmail.com>
2021-04-13 13:13:48 +0000

    [update]4-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1935f989d567c9f43be720f1d077d234314c1dcce33ec7c4af40b92a752471009c657d0850386d0dbdb2334decf14ed7c075a2a57cad81fcac6d1d09758e5acc'
